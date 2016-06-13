10ead55c4c42e4cf8aa7de1c133d41543eecbb12
Steve Theodore <steve@theodox.com>
2016-06-13 00:52:16 -0700

    fixed add step in publish; updated 'youkids'

--- pelican/publish.py
+++ pelican/publish.py
@@ -5,9 +5,11 @@ import os
 import datetime
 
 now = datetime.datetime.now()
+add_cmd = 'git add .'
 commit_cmd = 'git commit -a -m "published %s-%s-%s %s:%s"' % (now.year, now.month, now.day, now.hour, now.min)
 push_cmd = 'git push'
 
 os.chdir(sys.argv[1])
+os.system(add_cmd)
 os.system(commit_cmd)
 os.system(push_cmd)
