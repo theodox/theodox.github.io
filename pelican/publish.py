2911130ba1d15265e9c707d613fe5a522a4a1e89
Steve Theodore <steve@theodox.com>
2016-10-16 23:26:57 -0700

    force git pull before push to stay in sync

--- pelican/publish.py
+++ pelican/publish.py
@@ -5,11 +5,13 @@ import os
 import datetime
 
 now = datetime.datetime.now()
+pull_cmd = 'git pull'
 add_cmd = 'git add .'
 commit_cmd = 'git commit -a -m "published %s-%s-%s %s:%s"' % (now.year, now.month, now.day, now.hour, now.min)
 push_cmd = 'git push'
 
 os.chdir(sys.argv[1])
+os.system(pull_cms)
 os.system(add_cmd)
 os.system(commit_cmd)
 os.system(push_cmd)
