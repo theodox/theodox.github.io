4d24f206c9de54c1ba7b0a78eff419727fcacdd4
Steve Theodore <steve@theodox.com>
2017-03-31 23:30:02 -0700

    fix link?

---
+++ pelican/content/fix_slugs.py
@@ -0,0 +1,32 @@
+import os
+
+def fix_slug(s):
+	lowered = s.lower()
+	for char in ":.!?[]()& ":
+		lowered = lowered.replace(char, "_")
+	lowered = lowered.replace("__", "_")
+	lowered = lowered.replace("__", "_")
+	lowered = lowered.replace("__", "_")
+	lowered = lowered.replace("_md", "")
+	return lowered
+
+def rewrite(f):
+	result = []
+	with open(f, 'rt') as handle:
+		for line in handle:
+			if 'Slug:' in line:
+				s,_,rest = line.partition(":")
+				rest = fix_slug(rest)
+				line = "Slug: " + rest
+			result.append(line)
+
+	with open(f, 'wt') as handle:
+		handle.writelines(result)
+
+for file in os.listdir("."):
+	oldname = fix_slug(file)
+	print file, oldname
+	if file.endswith("md"):
+		rewrite(file)
+
+	os.rename(file, fix_slug(file) + ".md")
